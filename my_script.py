a92cb820022227ce81860063e30b32f7e0d52acd
HuanNing <gladcolor@gmail.com>
2019-11-10 14:15:52 -0500

    Task: added print info.

--- my_script.py
+++ my_script.py
@@ -3,5 +3,7 @@
 from pystrich.datamatrix import DataMatrixEncoder
 
 encoder = DataMatrixEncoder('This is a DataMatrix.')
-encoder.save('./datamatrix_test.png')
+encoder.save('./datamatrix_Street View Motion-from-Structure-from-Motiontest.png')
 print(encoder.get_ascii())
+
+print("This is a Docerk test for IS601001, Windows 10, by Huan")
